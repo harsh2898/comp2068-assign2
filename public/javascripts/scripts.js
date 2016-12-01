a6aeecc41e329d0d1ee326206763359bf3ffd575
harsh2898 <harshjpatel2898@yahoo.com>
2016-12-01 00:04:33 -0500

    Insert, Delete and Edit

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,26 @@
+/**
+ * Created by test on 21/10/2016.
+ */
+
+/* use jquery which we just downloaded
+ find any html element with the class "confirmation"
+ attach a js confirmation popup to the click event of these html elements
+ */
+$('.confirmation').on('click', function() {
+    return confirm('Are you sure you want to delete this?');
+});
+
+// password check on register Form
+var validator = $('#registerForm').validate({
+    rules: {
+        confirm: {
+            required: true,
+            equalTo: '#password'
+        }
+    },
+    messages: {
+        confirm: {
+            equalTo: 'Your passwords do not match'
+        }
+    }
+});
--- routes/assignments.js
+++ routes/assignments.js
@@ -7,11 +7,50 @@
 var express = require('express');
 var router = express.Router();
 
+//link to the assignments model
+var Assignment = require('../models/assignment');
+
 // GET main assignments page
 router.get('/', function(req, res, next) {
-    // load the assignments page
-    res.render('assignments', {
-        title: 'Assignment'
+
+    Assignment.find(function(err, assignments) {
+        if (err) {
+            console.log(err);
+            res.render('error');
+        }
+        else {
+            // load the assignments page and passing the result
+            res.render('assignments', {
+                title: 'Assignment',
+                assignments: assignments
+            });
+        }
+    });
+});
+
+/* GET /assignments/add */
+router.get('/add', function(req, res, next) {
+    res.render('add-assignment', { title: 'Add your Assignment'})
+});
+
+/* POST /assignments/add */
+router.post('/add', function(req, res, next) {
+    // get the form inputs and using mongoose to insert to the db
+    Assignment.create({
+        stuName: req.body.stuName,
+        course: req.body.course,
+        assignName: req.body.assignName,
+        dueDate: req.body.dueDate,
+        description: req.body.description,
+        weightage: req.body.weightage
+    }, function(err, Assignment) {
+        if (err) {
+            console.log(err);
+            res.render('error', { message: 'Could not add the Assignment.'});
+        }
+        else {
+            res.redirect('/assignments');
+        }
     });
 });
 
