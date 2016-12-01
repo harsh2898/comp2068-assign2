24725cdd7d3085fdf2904eeede504c1f6af7751b
harsh2898 <harshjpatel2898@yahoo.com>
2016-12-01 02:07:04 -0500

    edit and delete completed

--- routes/assignments.js
+++ routes/assignments.js
@@ -54,5 +54,79 @@ router.post('/add', function(req, res, next) {
     });
 });
 
+/* GET /assignments/delete/_id  - delete process */
+router.get('/delete/:_id', function(req, res, next) {
+    // get the id from the url
+    var _id = req.params._id;
+
+    //delete the assignment with this id
+    Assignment.remove({ _id: _id }, function(err) {
+        if (err) {
+            console.log(err);
+            res.render('error', {
+                message: 'Could not delete this Assignment',
+                error: err
+            });
+        }
+        else {
+            res.redirect('/assignments');
+        }
+    });
+});
+
+/* GET /assignments/_id - display edit and fill */
+router.get('/:_id', function(req, res, next) {
+    // get id from the url
+    var _id = req.params._id;
+
+    // use mongoose to get the selected assignment details
+    Assignment.findById({_id: _id}, function(err, assignment) {
+        if (err) {
+            console.log(err);
+            res.render('error', {
+                message: 'Could not Load Assignment',
+                error: err
+            });
+        }
+        else {
+            res.render('edit-assignment', {
+                title: 'Edit an Assignment',
+                assignment: assignment
+            });
+        }
+    });
+});
+
+/* POST /drinks/_id  - update and save the assignment */
+router.post('/:_id', function(req, res, next) {
+    // get the id from the url
+    var _id = req.params._id;
+
+    // instantiate and populate new assignment object
+    var assignment = new Assignment({
+        _id: _id,
+        stuName: req.body.stuName,
+        course: req.body.course,
+        assignName: req.body.assignName,
+        dueDate: req.body.dueDate,
+        description: req.body.description,
+        weightage: req.body.weightage
+    });
+
+    // update the assignment
+    Assignment.update({_id: _id}, assignment, function(err) {
+        if (err) {
+            console.log(err);
+            res.render('error', {
+                message: 'Could not update the Assignment',
+                error: err
+            });
+        }
+        else {
+            res.redirect('/assignments');
+        }
+    })
+});
+
 // making this public
 module.exports = router;
